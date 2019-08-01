ccd3254cacdc1c6e14c3e04662a809bd704d4c5a
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2019-08-01 18:00:32 -0300

    Adicionado travis build

---
+++ travis/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: build.sh
+# 
+#         USAGE: ./build.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Fernando Augusto Medeiros Silva (), fams@linuxplace.com.br
+#  ORGANIZATION: Linuxplace
+#       CREATED: 01/08/2019 17:58
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+go build  -o ext-auth-poc cmd/jwtgw/*.go
+

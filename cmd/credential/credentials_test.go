0c2f92894b0ace2c01767498e8107095d5b9ba63
Fernando Augusto Medeiros Silva <fernando.augusto@gmail.com>
2019-08-12 15:08:48 -0300

    Adicionados testes para o credentials JWT agora e um objeto

---
+++ cmd/credential/credentials_test.go
@@ -0,0 +1,27 @@
+package credential
+
+import (
+	"testing"
+	//"time"
+)
+
+func TestCredential(t *testing.T) {
+	bucket := "resultfams"
+	key := "credentials.json"
+	region := "us-east-1"
+
+	loader := S3loader{bucket, key, region}
+	//Carregando permissões iniciais
+	credentialMap := New(&loader)
+	fingerprint := "fingerprint1"
+	claims, okValidate := credentialMap.Validate(Permission{fingerprint, "f1scope"})
+	if !okValidate {
+		t.Errorf("Validação de fingerprint %s", fingerprint)
+	}
+
+	claim := "param-get"
+	if claims.Audience[0] != claim {
+		t.Errorf("Claim  %s nao encontrado", claim)
+	}
+
+}
--- cmd/credential/s3loader.go
+++ cmd/credential/s3loader.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 )
 
-
 type S3loader struct {
 	BucketName string
 	KeyName    string
@@ -19,16 +18,16 @@ type S3loader struct {
 }
 
 type Scope struct {
-	Name string
+	Name      string
 	Audiences []string
 }
 type Credential struct {
 	Fingerprint, Name string
-	Scopes []Scope
+	Scopes            []Scope
 }
 
 //Load Permissions from S3
-func (s *S3loader)LoadCredentials()(PermissionClaims, bool) {
+func (s *S3loader) LoadCredentials() (PermissionClaims, bool) {
 	//Nova Sessão com a AWS
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String(s.Region)},
@@ -37,7 +36,6 @@ func (s *S3loader)LoadCredentials()(PermissionClaims, bool) {
 	input := &s3.GetObjectInput{
 		Bucket: aws.String(s.BucketName),
 		Key:    aws.String(s.KeyName),
-
 	}
 	//Obtem json
 	result, err := svc.GetObject(input)
@@ -45,14 +43,14 @@ func (s *S3loader)LoadCredentials()(PermissionClaims, bool) {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
 			case s3.ErrCodeNoSuchKey:
-				log.Errorf(s3.ErrCodeNoSuchKey, aerr.Error())
+				log.Error(s3.ErrCodeNoSuchKey, aerr.Error())
 			default:
-				log.Errorf(aerr.Error())
+				log.Error(aerr.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
-			log.Errorf(err.Error())
+			log.Error(err.Error())
 		}
 		// retorna vazio se não consegue
 		return nil, false
@@ -69,7 +67,7 @@ func (s *S3loader)LoadCredentials()(PermissionClaims, bool) {
 			return nil, false
 		}
 		for i := 0; i < len(perm.Scopes); i++ {
-			log.Debugf("recebido Fingerprint %s, Scope	: %s, Claim: %s", perm.Fingerprint, perm.Scopes[i].Name,strings.Join(perm.Scopes[i].Audiences[:], "|"))
+			log.Debugf("recebido Fingerprint %s, Scope	: %s, Claim: %s", perm.Fingerprint, perm.Scopes[i].Name, strings.Join(perm.Scopes[i].Audiences[:], "|"))
 			pc[Permission{perm.Fingerprint, perm.Scopes[i].Name}] = Claims{perm.Scopes[i].Audiences}
 		}
 	}
